5e27e9ba0826f4d0d7c36577c20f1d1af1574a31
choonhows <cjtan2406@gmail.com>
2026-05-26 19:14:38 +0800

    fix: pin home_widget to 0.8.1 and force glance 1.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,16 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+configurations.configureEach {
+    resolutionStrategy {
+        force("androidx.glance:glance:1.0.0")
+        force("androidx.glance:glance-appwidget:1.0.0")
+        force("androidx.glance:glance-preview:1.0.0")
+        force("androidx.glance:glance-appwidget-preview:1.0.0")
+    }
 }
 
 android {
@@ -21,10 +29,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.caledoro"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,8 +38,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
